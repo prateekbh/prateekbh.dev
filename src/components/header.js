6423e49ff51810fc866d08c3300b9aaaf96eee2d
Prateek Bhatnagar <prateek89born@gmail.com>
2019-06-18 23:28:14 -0700

    adding header

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,15 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import style from './header.module.css';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header className={style.header}>
+    <nav className={style.headerContent}>
+      <a className={style.navLink} href="#">Blog</a>
+      <a className={style.navLink} href="#">Talks</a>
+      <a className={style.navLink} href="#">Contact</a>
+    </nav>
   </header>
 )
 
