7596ce68c7fef6de708ea61f5a559db5cc37c912
Prateek Bhatnagar <prateek89born@gmail.com>
2019-06-18 23:51:59 -0700

    adding name and sample phoenatics

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
 import './app.css';
+import style from './index.module.css';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-
+    <div>
+      <h1 className={style.name}>Prateek Bhatnagar</h1>
+      <div className={style.phoenatics}>[Pratëëk - Bhätnägär]</div>
+    </div>
   </Layout>
 )
 
