66e88c2997dc05eee71fe55087c24c5cc7f2634c
Prateek Bhatnagar <prateek89born@gmail.com>
2019-06-29 11:39:47 -0700

    blogsss

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/_posts/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,16 +1,49 @@
-import React, {useEffect} from "react"
-
+import React, { useEffect } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link, graphql, StaticQuery } from "gatsby"
 
-
-const IndexPage = () => {
+const BlogRoll = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark
+  console.log({ posts })
   return (
     <Layout>
       <SEO title="Blogs" />
       <h1>All the blogs</h1>
+      {posts.map(({ node }) => {
+        const { excerpt, id } = node;
+        const {frontmatter: {date, title}} = node;
+        return (
+          <Link to={`/blog/${id}`} key={id}>
+            <div>
+              <h2>- {title}</h2>
+              <span>{date}</span>
+            </div>
+          </Link>
+        );
+      })}
     </Layout>
-  );
+  )
 }
 
-export default IndexPage
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query BlogRollQuery {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+          edges {
+            node {
+              frontmatter {
+                date
+                title
+              }
+              excerpt(pruneLength: 400)
+              id
+            }
+          }
+        }
+      }
+    `}
+    render={(data, count) => <BlogRoll data={data} count={count} />}
+  />
+)
