9ce2c5b9d560851307b2894f6105d3781e27ae25
Prateek Bhatnagar <prateek89born@gmail.com>
2019-10-17 11:28:22 -0700

    new preact blog

--- next.config.js
+++
@@ -1,8 +0,0 @@
-// next.config.js
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
-  cssModules: true,
-  env: {
-    GH_TOKEN: process.env.GH_TOKEN
-  }
-})
--- src/layouts/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Header from '../components/header'
-export default ({ children }) => (
-  <div>
-    <Header />
-    { children }
-  </div>
-)
