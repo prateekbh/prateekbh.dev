e3fe8725df7be6c672f201cbe893f85884c8b25c
Prateek Bhatnagar <prateek89born@gmail.com>
2020-01-05 15:28:02 -0800

    fixing code highlight

--- src/routes/blog/index.js
+++ src/routes/blog/index.js
@@ -1,7 +1,20 @@
 import { h } from 'preact';
+import { useEffect } from 'preact/hooks';
 import { usePrerenderData } from '@preact/prerender-data-provider';
 import Markdown from 'markdown-to-jsx';
+import hljs from 'highlight.js/lib/highlight';
+import ts from 'highlight.js/lib/languages/typescript';
+import javascript from 'highlight.js/lib/languages/javascript';
+import html from 'highlight.js/lib/languages/htmlbars';
+import css from 'highlight.js/lib/languages/css';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('typescript', ts);
+hljs.registerLanguage('html', html);
+hljs.registerLanguage('css', css);
+
 import style from './style';
+import 'highlight.js/styles/github.css';
 
 const blogs = (props) => {
 	const [data, isLoading] = usePrerenderData(props);
@@ -12,7 +25,24 @@ const blogs = (props) => {
 	);
 };
 
+function CodeBlock(props) {
+	return <pre><code>{props.children}</code></pre>
+}
+
 function getBlogBody(data, isLoading) {
+	let blogDiv;
+	useEffect(() => {
+		if (blogDiv) {
+			//hljs.configure({ useBR: true });
+			setTimeout(() => {
+				blogDiv.querySelectorAll('code').forEach((block) => {
+					hljs.highlightBlock(block);
+				});
+			}, 100);
+		}
+	}, []);
+
+
 	if (isLoading) {
 		return (
 			<div class={style.loadingPlaceholder}>
@@ -34,8 +64,14 @@ function getBlogBody(data, isLoading) {
 				<h1 class={style.blogtitle}>{details.title}</h1>
 				{ details.subtitle && <caption class={style.blogsubtitle}>{details.subtitle}</caption> }
 				{ details.cover && <div class={style.blogcover} style={`background-image:url(${details.cover})`} /> }
-				<div class={style.blogbody}>
-					<Markdown>{ content }</Markdown>
+				<div class={style.blogbody} ref={c => blogDiv = c}>
+					<Markdown options={{
+            overrides: {
+                code: {
+                    component: CodeBlock,
+                },
+            },
+        }}>{ content }</Markdown>
 				</div>
 			</div>
 		);
