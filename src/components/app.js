7f990cff5ac202300a4035f321b4009a846bb0c3
Prateek Bhatnagar <prateek89born@gmail.com>
2020-01-13 14:34:23 -0800

    removing gid

--- src/components/app.js
+++ src/components/app.js
@@ -27,7 +27,7 @@ export default class App extends Component {
 	constructor() {
 		super();
 		if (typeof window !== 'undefined') {
-			window.ga = new GAnalytics('UA-91863394-2', { aid: 1 });
+			window.ga = new GAnalytics('UA-91863394-2');
 		}
 	}
 
